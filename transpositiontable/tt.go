badebd64495ff701dc06f1d0810d39cd1f01e0dc
Frank Kopp <frank@familie-kopp.de>
2020-03-18 12:39:03 +0100

    Started TT - get, probe, age, clear

--- transpositiontable/tt.go
+++ transpositiontable/tt.go
@@ -26,6 +26,8 @@ package transpositiontable
 
 import (
 	"math"
+	"sync"
+	"time"
 	"unsafe"
 
 	"golang.org/x/text/language"
@@ -42,12 +44,12 @@ var log = franky_logging.GetLog("tt")
 // TtEntry struct is the data structure for each entry in the transposition
 // table. Each entry has 16-bytes (128-bits)
 type TtEntry struct {
-	Key        position.Key // 64-bit Zobrist Key
-	Move       types.Move   // 32-bit Move and Value
-	Depth      int8         // 7-bit 0-127 0b01111111
-	Age        int8         // 3-bit 0-7   0b00000111 0=used 1=generated, not used, >1 older generation
-	Type       int8         // 2-bit None, Exact, Alpha (upper), Beta (lower)
-	MateThreat bool         // 1-bit
+	Key        position.Key    // 64-bit Zobrist Key
+	Move       types.Move      // 32-bit Move and Value
+	Depth      int8            // 7-bit 0-127 0b01111111
+	Age        int8            // 3-bit 0-7   0b00000111 0=used 1=generated, not used, >1 older generation
+	Type       types.ValueType // 2-bit None, Exact, Alpha (upper), Beta (lower)
+	MateThreat bool            // 1-bit
 }
 
 const (
@@ -66,6 +68,15 @@ type TtTable struct {
 	hashKeyMask        uint64
 	maxNumberOfEntries uint64
 	numberOfEntries    uint64
+
+	// statistics
+	numberOfPuts       uint64
+	numberOfCollisions uint64
+	numberOfOverwrites uint64
+	numberOfUpdates    uint64
+	numberOfProbes     uint64
+	numberOfHits       uint64
+	numberOfMisses     uint64
 }
 
 // New creates a new TtTable with the given number of bytes
@@ -80,22 +91,22 @@ func New(sizeInMByte int) *TtTable {
 		numberOfEntries:    0,
 	}
 
-	tt.resize(sizeInMByte)
+	tt.Resize(sizeInMByte)
 
 	return &tt
 }
 
-func (tt *TtTable) resize(sizeInMByte int) {
+// Resize resizes the tt table. All entries will be cleared.
+func (tt *TtTable) Resize(sizeInMByte int) {
 	if sizeInMByte > MaxSizeInMB {
 		log.Error(out.Sprintf("Requested size for TT of %d MB reduced to max of %d MB", sizeInMByte, MaxSizeInMB))
 		sizeInMByte = MaxSizeInMB
 	}
 
-	tt.sizeInByte = uint64(sizeInMByte) * types.MB
-
 	// calculate the maximum power of 2 of entries fitting into the given size in MB
+	tt.sizeInByte = uint64(sizeInMByte) * types.MB
 	tt.maxNumberOfEntries = 1 << uint64(math.Floor(math.Log2(float64(tt.sizeInByte/TtEntrySize))))
-	tt.hashKeyMask = tt.maxNumberOfEntries -1 // --> 0x0001111....111
+	tt.hashKeyMask = tt.maxNumberOfEntries - 1 // --> 0x0001111....111
 
 	// if TT is resized to 0 we cant have any entries.
 	if tt.sizeInByte == 0 {
@@ -105,8 +116,89 @@ func (tt *TtTable) resize(sizeInMByte int) {
 	// calculate the real memory usage
 	tt.sizeInByte = tt.maxNumberOfEntries * TtEntrySize
 
-	tt.data = make([]TtEntry, 0, tt.maxNumberOfEntries)
+	// Create new slice/array - garbage collections takes care of cleanup
+	tt.data = make([]TtEntry, tt.maxNumberOfEntries, tt.maxNumberOfEntries)
 
 	log.Info(out.Sprintf("TT Size %d MByte, Capacity %d entries (size=%dByte) (Requested were %d MBytes)",
-		tt.sizeInByte / types.MB, tt.maxNumberOfEntries, unsafe.Sizeof(TtEntry{}), sizeInMByte))
+		tt.sizeInByte/types.MB, tt.maxNumberOfEntries, unsafe.Sizeof(TtEntry{}), sizeInMByte))
+}
+
+// GetEntry returns a pointer to the corresponding tt entry
+// or nil if it was not found
+func (tt *TtTable) GetEntry(key position.Key) *TtEntry {
+	e := &tt.data[tt.hash(key)]
+	if e.Key == key {
+		return e
+	}
+	return nil
+}
+
+// Probe returns a pointer to the corresponding tt entry
+// or nil if it was not found. Decreases TtEntry.Age by 1
+func (tt *TtTable) Probe(key position.Key) *TtEntry {
+	e := &tt.data[tt.hash(key)]
+	if e.Key == key {
+		e.Age--
+		if e.Age < 0 {
+			e.Age = 0
+		}
+		return e
+	}
+	return nil
+}
+
+// Clear clears all entries of the tt
+func (tt *TtTable) Clear() {
+	// Create new slice/array - garbage collections takes care of cleanup
+	tt.data = make([]TtEntry, tt.maxNumberOfEntries, tt.maxNumberOfEntries)
+	tt.numberOfPuts = 0
+	tt.numberOfEntries = 0
+	tt.numberOfHits = 0
+	tt.numberOfUpdates = 0
+	tt.numberOfMisses = 0
+	tt.numberOfCollisions = 0
+	tt.numberOfOverwrites = 0
+	tt.numberOfProbes = 0
+}
+
+// Hashfull returns how full the transposition table is in permill as per UCI
+func (tt *TtTable) Hashfull() int {
+	if tt.maxNumberOfEntries == 0 {
+		return 0
+	}
+	return int((1000 * tt.numberOfEntries) / tt.maxNumberOfEntries)
+}
+
+// hash generates the internal hash key for the data array
+func (tt *TtTable) hash(key position.Key) uint64 {
+	return uint64(key) & tt.hashKeyMask
+}
+
+// ages each entry in the tt
+// Creates a number of go routines with each a certain slice of data
+// to process
+func (tt *TtTable) ageEntries() {
+	numberOfGoroutines := uint64(8)
+	startTime := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(int(numberOfGoroutines))
+	slice := tt.maxNumberOfEntries / numberOfGoroutines
+	for i := uint64(0); i < numberOfGoroutines; i++ {
+		go func(i uint64) {
+			defer wg.Done()
+			start := i * slice
+			end := start + slice
+			if i == numberOfGoroutines-1 {
+				end = tt.maxNumberOfEntries
+			}
+			for n := start; n < end; n++ {
+				if tt.data[n].Key != 0 {
+					tt.data[n].Age++
+				}
+			}
+		}(i)
+	}
+	wg.Wait()
+	elapsed := time.Since(startTime)
+	log.Debug(out.Sprintf("Aged %d entries in %d ms\n", len(tt.data), elapsed.Milliseconds()))
 }
--- transpositiontable/tt_test.go
+++ transpositiontable/tt_test.go
@@ -26,12 +26,14 @@ package transpositiontable
 
 import (
 	"testing"
+	"time"
 	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/frankkopp/FrankyGo/franky_logging"
-	"github.com/frankkopp/FrankyGo/types"
+	"github.com/frankkopp/FrankyGo/position"
+	. "github.com/frankkopp/FrankyGo/types"
 )
 
 var logTest = franky_logging.GetLog("test")
@@ -63,5 +65,117 @@ func TestNew(t *testing.T) {
 	tt = New(35_000)
 	assert.Equal(t, uint64(2_147_483_648), tt.maxNumberOfEntries)
 	assert.Equal(t, 2_147_483_648, cap(tt.data))
-	assert.Equal(t, 32_768*types.MB, tt.sizeInByte)
+	assert.Equal(t, 2_147_483_648, len(tt.data))
+	assert.Equal(t, 32_768*MB, tt.sizeInByte)
+	for i, _ := range tt.data {
+		tt.data[i].Key = position.Key(i)
+	}
+	assert.Equal(t, position.Key(0), tt.data[0].Key)
+	assert.Equal(t, position.Key(2_147_483_647), tt.data[2_147_483_647].Key)
 }
+
+func TestGetAndProbe(t *testing.T) {
+	// setup
+	tt := New(64)
+	assert.Equal(t, uint64(4_194_304), tt.maxNumberOfEntries)
+	assert.Equal(t, 4_194_304, cap(tt.data))
+
+	pos := position.New()
+	move := CreateMove(SqE2, SqE4, Normal, PtNone)
+	tt.data[tt.hash(pos.ZobristKey())] = TtEntry{
+		Key:        pos.ZobristKey(),
+		Move:       move,
+		Depth:      5,
+		Age:        1,
+		Type:       Vnone,
+		MateThreat: false,
+	}
+	tt.numberOfEntries++
+
+	// test to get unaltered entry
+	e := tt.GetEntry(pos.ZobristKey())
+	assert.Equal(t, pos.ZobristKey(), e.Key )
+	assert.Equal(t, move, e.Move)
+	assert.EqualValues(t, 5, e.Depth)
+	assert.EqualValues(t, 1, e.Age)
+	assert.Equal(t, Vnone, e.Type)
+
+	// age must be reduced by 1
+	e = tt.Probe(pos.ZobristKey())
+	assert.Equal(t, pos.ZobristKey(), e.Key )
+	assert.Equal(t, move, e.Move)
+	assert.EqualValues(t, 5, e.Depth)
+	assert.EqualValues(t, 0, e.Age)
+	assert.Equal(t, Vnone, e.Type)
+
+	// age does not go below 0
+	e = tt.Probe(pos.ZobristKey())
+	assert.EqualValues(t, 0, e.Age)
+
+	// not in tt
+	pos.DoMove(move)
+	e = tt.Probe(pos.ZobristKey())
+	assert.Nil(t, e)
+}
+
+func TestClear(t *testing.T) {
+	// setup
+	tt := New(1)
+
+	pos := position.New()
+	move := CreateMove(SqE2, SqE4, Normal, PtNone)
+	tt.data[tt.hash(pos.ZobristKey())] = TtEntry{
+		Key:        pos.ZobristKey(),
+		Move:       move,
+		Depth:      5,
+		Age:        1,
+		Type:       Vnone,
+		MateThreat: false,
+	}
+	tt.numberOfEntries++
+
+	e := tt.Probe(pos.ZobristKey())
+	assert.Equal(t, pos.ZobristKey(), e.Key )
+	assert.Equal(t, move, e.Move)
+	assert.EqualValues(t, 5, e.Depth)
+	assert.EqualValues(t, 0, e.Age)
+	assert.Equal(t, Vnone, e.Type)
+	assert.EqualValues(t, 1, tt.numberOfEntries)
+
+	tt.Clear()
+
+	// entry is gone
+	e = tt.Probe(pos.ZobristKey())
+	assert.Nil(t, e)
+	assert.EqualValues(t, 0, tt.numberOfEntries)
+}
+
+func TestAge(t *testing.T) {
+	// setup
+	tt := New(5_000)
+
+	logTest.Debug("Filling tt")
+	startTime := time.Now()
+	for i, _ := range tt.data {
+		tt.data[i].Key = position.Key(i)
+		tt.data[i].Age++
+	}
+	tt.data[0].Age = 0
+	elapsed := time.Since(startTime)
+	logTest.Debug(out.Sprintf("TT of %d elements filled in %d ms\n", len(tt.data), elapsed.Milliseconds()))
+
+	// test
+	assert.EqualValues(t, 0, tt.GetEntry(0).Age)
+	assert.EqualValues(t, 1, tt.GetEntry(1).Age)
+	assert.EqualValues(t, 1, tt.GetEntry(1_000).Age)
+	assert.EqualValues(t, 1, tt.GetEntry(position.Key(tt.maxNumberOfEntries-1)).Age)
+
+	logTest.Debug("Ageing entries")
+	tt.ageEntries()
+
+	assert.EqualValues(t, 0, tt.GetEntry(0).Age)
+	assert.EqualValues(t, 2, tt.GetEntry(1).Age)
+	assert.EqualValues(t, 2, tt.GetEntry(1_000).Age)
+	assert.EqualValues(t, 2, tt.GetEntry(position.Key(tt.maxNumberOfEntries-1)).Age)
+}
+
