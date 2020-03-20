5cc028642607c37a935a8b35b2b5bd11a4fc3a34
Frank Kopp <frank@familie-kopp.de>
2020-03-20 15:40:14 +0100

    Start v0.3

--- FrankyGo.go
+++ FrankyGo.go
@@ -8,7 +8,7 @@ import (
 import "github.com/frankkopp/FrankyGo/uci"
 
 func main() {
-	fmt.Println("FrankyGo")
+	fmt.Println("FrankyGo v0.3 (dev started 20.3.2020)")
 
 	types.Init()
 	u := uci.NewUciHandler()
