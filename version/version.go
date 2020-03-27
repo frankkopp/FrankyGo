8b2ac3631a6ff523a89c089a9122bb0b1709fe3f
Frank Kopp <frank@familie-kopp.de>
2020-03-27 17:26:59 +0100

    start dev0.5

--- version/version.go
+++ version/version.go
@@ -33,9 +33,9 @@ import (
 // Version information as constants
 const (
 	Major  = 0
-	Minor  = 4
-	Status = "rc"
-	Date   = "25.03.2020"
+	Minor  = 5
+	Status = "dev"
+	Date   = "27.03.2020"
 )
 
 // Version returns the version of the application as a string
