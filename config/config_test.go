650aba6d07b808176ed72bbc4f38bace49c1421e
Frank Kopp <frank@familie-kopp.de>
2020-04-17 01:17:29 +0200

    CI tests

--- config/config_test.go
+++ config/config_test.go
@@ -28,8 +28,21 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"runtime"
 	"testing"
 )
+
+// make tests run in the projects root directory
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestInit(t *testing.T) {
 	Setup()
