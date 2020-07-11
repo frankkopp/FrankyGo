eb612a2f4082f2fa98b3a6dd9fbdc8c7a8252c54
Frank Kopp <frank@familie-kopp.de>
2020-07-11 02:16:01 +0200

    Fix tests

--- internal/uci/uci_test.go
+++ internal/uci/uci_test.go
@@ -326,7 +326,7 @@ func TestBookMove(t *testing.T) {
 	result = uh.Command("isready")
 	assert.Contains(t, result, "readyok")
 
-	result = uh.Command("setoption name Use_Book value true")
+	result = uh.Command("setoption name OwnBook value true")
 
 	uh.Command("position startpos moves e2e4 e7e5")
 	assert.EqualValues(t, "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2", uh.myPosition.StringFen())
